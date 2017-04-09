4f2b3bcbe4e8dda0aada4a2febb6294234241dc4
Lungu Andrei <h3nnry84@gmail.com>
2017-04-10 02:06:59 +0300

    Added support foundation scss

--- app/app.jsx
+++ app/app.jsx
@@ -6,7 +6,7 @@ var Countdown = require('Countdown');
 var Timer = require('Timer');
 
 // Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css');
+// require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 // App css
